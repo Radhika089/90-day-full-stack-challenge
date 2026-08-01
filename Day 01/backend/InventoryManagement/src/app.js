a223b7adbc4abf908ccbcefbc6736f3edc6b046b
Radhika <radhikabansal365@gmail.com>
2026-08-01 20:41:27 +0530

    feat: implement user authentication with registration and login endpoints

--- Day 01/backend/InventoryManagement/src/app.js
+++ Day 01/backend/InventoryManagement/src/app.js
@@ -1,8 +1,11 @@
 import express from "express";
+import userRouter from "./routes/user.routes.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+app.use("/auth/register", userRouter);
 
 export default app;
