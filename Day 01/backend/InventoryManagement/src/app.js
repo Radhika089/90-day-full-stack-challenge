6a6fe2c3b9cb1ff5373fe30be45effacb2f4b739
Radhika <radhikabansal365@gmail.com>
2026-08-02 08:00:21 +0530

    feat: configure dotenv and add cookie-parser middleware for improved request handling

--- Day 01/backend/InventoryManagement/src/app.js
+++ Day 01/backend/InventoryManagement/src/app.js
@@ -1,11 +1,15 @@
 import express from "express";
 import userRouter from "./routes/user.routes.js";
+import cookieParser from "cookie-parser";
+import productRouter from "./routes/product.routes.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/api/auth", userRouter);
+app.use("/api/product", productRouter);
 
 export default app;
