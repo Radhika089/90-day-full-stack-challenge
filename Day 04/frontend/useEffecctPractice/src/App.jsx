7c8d2aa6c3d779edf161b7f0b14aa4a85770e25b
Radhika <radhikabansal365@gmail.com>
2026-08-09 10:19:23 +0530

    feat: implement Task4TimerCleanup component with timer functionality and cleanup effect; update App to include Task4TimerCleanup

--- Day 04/frontend/useEffecctPractice/src/App.jsx
+++ Day 04/frontend/useEffecctPractice/src/App.jsx
@@ -2,13 +2,15 @@ import React from "react";
 import Task1EffectRuns from "./components/Task1EffectRuns";
 import Task2ApiFetch from "./components/Task2ApiFetch";
 import Task3SearchEffect from "./components/Task3SearchEffect";
+import Task4TimerCleanup from "./components/Task4TimerCleanup";
 
 const App = () => {
   return (
     <div>
       {/* <Task1EffectRuns /> */}
       {/* <Task2ApiFetch /> */}
-      <Task3SearchEffect />
+      {/* <Task3SearchEffect /> */}
+      <Task4TimerCleanup />
     </div>
   );
 };
--- Day 04/frontend/useEffecctPractice/src/components/Task4TimerCleanup.jsx
+++ Day 04/frontend/useEffecctPractice/src/components/Task4TimerCleanup.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
 const Task4TimerCleanup = () => {
-  return <div>Task4TimerCleanup</div>;
+  const [seconds, setSeconds] = useState(0);
+  const [running, setRunning] = useState(false);
+
+  useEffect(() => {
+    if (!running) return;
+
+    const interval = setInterval(() => {
+      setSeconds((prev) => prev + 1);
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, [running]);
+
+  return (
+    <div>
+      {" "}
+      <div>
+        <h1>Seconds: {seconds}</h1>
+
+        <button onClick={() => setRunning(true)}>Start</button>
+        <button onClick={() => setRunning(false)}>Stop</button>
+        <button onClick={() => setSeconds(0)}>Reset</button>
+      </div>
+    </div>
+  );
 };
 
 export default Task4TimerCleanup;
