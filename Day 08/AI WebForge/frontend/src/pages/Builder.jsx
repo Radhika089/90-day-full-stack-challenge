6033e3ff2f25c86cd7df056deb0bfe426e9ee3e4
Radhika <radhikabansal365@gmail.com>
2026-08-27 13:01:40 +0530

    feat: integrate FileExplorer component into Builder page for file management

--- Day 08/AI WebForge/frontend/src/pages/Builder.jsx
+++ Day 08/AI WebForge/frontend/src/pages/Builder.jsx
@@ -5,6 +5,7 @@ import Loading from "../components/Loading";
 import BuilderHeader from "../components/BuilderHeader";
 import { FolderTreeIcon, MessageSquareIcon } from "lucide-react";
 import ChatPanel from "../components/ChatPanel";
+import FileExplorer from "../components/FileExplorer";
 
 const Builder = () => {
   const { id } = useParams();
@@ -96,20 +97,34 @@ const Builder = () => {
           {/* sidebar content */}
           <div className="flex-1 overflow-hidden">
             {leftTab === "chat" ? (
-              <div className="h-full">
-                <ChatPanel
-                  messages={activeProject.messages}
-                  onSend={handleChat}
-                  loading={chatLoading}
-                />
-              </div>
+              <ChatPanel
+                messages={activeProject.messages}
+                onSend={handleChat}
+                loading={chatLoading}
+              />
             ) : (
-              <div>File Explorer</div>
+              <FileExplorer
+                files={activeProject.files}
+                activeFile={activeFile}
+                onFileSelect={(path) => {
+                  setActiveFile(path);
+                  setShowCode(true);
+                }}
+              />
             )}
           </div>
         </div>
 
         {/* Preview / Code Area */}
+        <div className="flex-1 overflow-hidden">
+          {activeProject.status === "pending" ||
+          activeProject.status === "generating" ||
+          activeProject.status === "failed" ? (
+            <Loading />
+          ) : (
+            <p>Preview Panel</p>
+          )}
+        </div>
       </div>
     </div>
   );
